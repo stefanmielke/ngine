1b112de0812e0e69fc7d0c08bb3f480700ba5571
Stefan Mielke <stefanmielke@fastmail.com>
2022-02-01 20:37:34 -0300

    Show output console (still no real output) and adding 'new project' option.

---
+++ console_app.h
@@ -0,0 +1,159 @@
+#pragma once
+
+#include <cstring>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+#include "imgui.h"
+
+struct ConsoleApp {
+	char InputBuf[256];
+	ImVector<char *> Items;
+	ImGuiTextFilter Filter;
+	bool ScrollToBottom;
+
+	ConsoleApp() {
+		ClearLog();
+		memset(InputBuf, 0, sizeof(InputBuf));
+
+		ScrollToBottom = false;
+	}
+	~ConsoleApp() {
+		ClearLog();
+	}
+
+	// Portable helpers
+	static int Stricmp(const char *s1, const char *s2) {
+		int d;
+		while ((d = toupper(*s2) - toupper(*s1)) == 0 && *s1) {
+			s1++;
+			s2++;
+		}
+		return d;
+	}
+	static int Strnicmp(const char *s1, const char *s2, int n) {
+		int d = 0;
+		while (n > 0 && (d = toupper(*s2) - toupper(*s1)) == 0 && *s1) {
+			s1++;
+			s2++;
+			n--;
+		}
+		return d;
+	}
+	static char *Strdup(const char *s) {
+		IM_ASSERT(s);
+		size_t len = strlen(s) + 1;
+		void *buf = malloc(len);
+		IM_ASSERT(buf);
+		return (char *)memcpy(buf, (const void *)s, len);
+	}
+	static void Strtrim(char *s) {
+		char *str_end = s + strlen(s);
+		while (str_end > s && str_end[-1] == ' ')
+			str_end--;
+		*str_end = 0;
+	}
+
+	void ClearLog() {
+		for (int i = 0; i < Items.Size; i++)
+			free(Items[i]);
+		Items.clear();
+	}
+
+	void AddLog(const char *fmt, ...) IM_FMTARGS(2) {
+		// FIXME-OPT
+		char buf[1024];
+		va_list args;
+		va_start(args, fmt);
+		vsnprintf(buf, IM_ARRAYSIZE(buf), fmt, args);
+		buf[IM_ARRAYSIZE(buf) - 1] = 0;
+		va_end(args);
+		Items.push_back(Strdup(buf));
+	}
+
+	void Draw(const char *title, sf::RenderWindow &window) {
+		ImGui::SetNextWindowSize(ImVec2(window.getSize().x, 200));
+		ImGui::SetNextWindowPos(ImVec2(0, window.getSize().y - 200));
+		if (!ImGui::Begin(title, nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar)) {
+			ImGui::End();
+			return;
+		}
+
+		// TODO: display items starting from the bottom
+		ImGui::TextUnformatted("Output");
+		ImGui::SameLine();
+		if (ImGui::SmallButton("Clear")) {
+			ClearLog();
+		}
+		ImGui::Separator();
+
+		ImGui::BeginChild("ScrollingRegion", ImVec2(0, 0), false,
+						  ImGuiWindowFlags_HorizontalScrollbar);
+		if (ImGui::BeginPopupContextWindow()) {
+			if (ImGui::Selectable("Clear"))
+				ClearLog();
+			ImGui::EndPopup();
+		}
+
+		// Display every line as a separate entry so we can change their color or add custom
+		// widgets. If you only want raw text you can use ImGui::TextUnformatted(log.begin(),
+		// log.end()); NB- if you have thousands of entries this approach may be too inefficient and
+		// may require user-side clipping to only process visible items. The clipper will
+		// automatically measure the height of your first item and then "seek" to display only items
+		// in the visible area. To use the clipper we can replace your standard loop:
+		//      for (int i = 0; i < Items.Size; i++)
+		//   With:
+		//      ImGuiListClipper clipper;
+		//      clipper.Begin(Items.Size);
+		//      while (clipper.Step())
+		//         for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; i++)
+		// - That your items are evenly spaced (same height)
+		// - That you have cheap random access to your elements (you can access them given their
+		// index,
+		//   without processing all the ones before)
+		// You cannot this code as-is if a filter is active because it breaks the 'cheap
+		// random-access' property. We would need random-access on the post-filtered list. A typical
+		// application wanting coarse clipping and filtering may want to pre-compute an array of
+		// indices or offsets of items that passed the filtering test, recomputing this array when
+		// user changes the filter, and appending newly elements as they are inserted. This is left
+		// as a task to the user until we can manage to improve this example code! If your items are
+		// of variable height:
+		// - Split them into same height items would be simpler and facilitate random-seeking into
+		// your list.
+		// - Consider using manual call to IsRectVisible() and skipping extraneous decoration from
+		// your items.
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1));  // Tighten spacing
+
+		for (int i = 0; i < Items.Size; i++) {
+			const char *item = Items[i];
+			if (!Filter.PassFilter(item))
+				continue;
+
+			// Normally you would store more information in your item than just a string.
+			// (e.g. make Items[] an array of structure, store color/type etc.)
+			ImVec4 color;
+			bool has_color = false;
+			if (strstr(item, "[error]")) {
+				color = ImVec4(1.0f, 0.4f, 0.4f, 1.0f);
+				has_color = true;
+			} else if (strncmp(item, "# ", 2) == 0) {
+				color = ImVec4(1.0f, 0.8f, 0.6f, 1.0f);
+				has_color = true;
+			}
+			if (has_color)
+				ImGui::PushStyleColor(ImGuiCol_Text, color);
+			ImGui::TextUnformatted(item);
+			if (has_color)
+				ImGui::PopStyleColor();
+		}
+
+		if (ScrollToBottom || (ImGui::GetScrollY() >= ImGui::GetScrollMaxY()))
+			ImGui::SetScrollHereY(1.0f);
+		ScrollToBottom = false;
+
+		ImGui::PopStyleVar();
+		ImGui::EndChild();
+
+		ImGui::End();
+	}
+};
--- main.cpp
+++ main.cpp
@@ -6,14 +6,15 @@
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 
+#include "console_app.h"
+
+ConsoleApp console;
+
 int main() {
 	sf::RenderWindow window(sf::VideoMode(1024, 768), "NGine - N64 Engine Powered by Libdragon");
 	window.setFramerateLimit(60);
 	if (!ImGui::SFML::Init(window))
 		return -1;
-
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
 
 	sf::Clock deltaClock;
 	while (window.isOpen()) {
@@ -28,12 +29,47 @@ int main() {
 
 		ImGui::SFML::Update(window, deltaClock.restart());
 
-		ImGui::Begin("Hello, world!");
-		ImGui::Button("Look at this pretty button");
-		ImGui::End();
+		static bool new_project_window_open = false;
+		if (ImGui::BeginMainMenuBar()) {
+			if (ImGui::BeginMenu("File")) {
+				if (ImGui::MenuItem("New Project")) {
+					new_project_window_open = true;
+				}
+				if (ImGui::MenuItem("Exit")) {
+					window.close();
+				}
+				ImGui::EndMenu();
+			}
+			ImGui::EndMainMenuBar();
+		}
+
+		if (new_project_window_open) {
+			ImGui::SetNextWindowSize(ImVec2(300, 80));
+			if (ImGui::Begin("New Project")) {
+				char input[255];
+				ImGui::TextUnformatted("Folder");
+				ImGui::SameLine();
+				ImGui::InputText("##", input, 255);
+				if (ImGui::Button("Create Project", ImVec2(50, 20))) {
+					char command[500];
+					snprintf(command, 500, "cd %s\nlibdragon init", input);
+					system(command);
+					new_project_window_open = false;
+				}
+				ImGui::SameLine();
+				if (ImGui::Button("Cancel", ImVec2(50, 20))) {
+					new_project_window_open = false;
+				}
+				ImGui::End();
+			}
+		}
+
+		console.Draw("Console", window);
 
 		window.clear();
-		window.draw(shape);
+
+		// render SFML stuff here
+
 		ImGui::SFML::Render(window);
 		window.display();
 	}
