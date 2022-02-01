c2a231144ccef4e2e58acf9ff23441a522285065
Stefan Mielke <stefanmielke@fastmail.com>
2022-02-01 19:39:35 -0300

    First commit with SFML and ImGui working.

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "imgui/imgui.h"
+#include "imgui-SFML/imgui-SFML.h"
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(1024, 768), "NGine - N64 Engine Powered by Libdragon");
+	window.setFramerateLimit(60);
+	if (!ImGui::SFML::Init(window))
+		return -1;
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	sf::Clock deltaClock;
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(window, event);
+
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::Begin("Hello, world!");
+		ImGui::Button("Look at this pretty button");
+		ImGui::End();
+
+		window.clear();
+		window.draw(shape);
+		ImGui::SFML::Render(window);
+		window.display();
+	}
+
+	ImGui::SFML::Shutdown();
+
+	return 0;
+}
