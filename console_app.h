50f0786fb7317b2c3fff631d2051fb96449f17be
Stefan Mielke <stefanmielke@fastmail.com>
2022-02-01 21:23:32 -0300

    Adding project creation using 'libdragon init'.

--- console_app.h
+++ console_app.h
@@ -7,14 +7,12 @@
 #include "imgui.h"
 
 struct ConsoleApp {
-	char InputBuf[256];
 	ImVector<char *> Items;
 	ImGuiTextFilter Filter;
 	bool ScrollToBottom;
 
 	ConsoleApp() {
 		ClearLog();
-		memset(InputBuf, 0, sizeof(InputBuf));
 
 		ScrollToBottom = false;
 	}
@@ -23,35 +21,12 @@ struct ConsoleApp {
 	}
 
 	// Portable helpers
-	static int Stricmp(const char *s1, const char *s2) {
-		int d;
-		while ((d = toupper(*s2) - toupper(*s1)) == 0 && *s1) {
-			s1++;
-			s2++;
-		}
-		return d;
-	}
-	static int Strnicmp(const char *s1, const char *s2, int n) {
-		int d = 0;
-		while (n > 0 && (d = toupper(*s2) - toupper(*s1)) == 0 && *s1) {
-			s1++;
-			s2++;
-			n--;
-		}
-		return d;
-	}
 	static char *Strdup(const char *s) {
 		IM_ASSERT(s);
 		size_t len = strlen(s) + 1;
 		void *buf = malloc(len);
 		IM_ASSERT(buf);
 		return (char *)memcpy(buf, (const void *)s, len);
-	}
-	static void Strtrim(char *s) {
-		char *str_end = s + strlen(s);
-		while (str_end > s && str_end[-1] == ' ')
-			str_end--;
-		*str_end = 0;
 	}
 
 	void ClearLog() {
--- main.cpp
+++ main.cpp
@@ -7,10 +7,36 @@
 #include <SFML/Window/Event.hpp>
 
 #include "console_app.h"
+#include <sstream>
 
 ConsoleApp console;
 
+#include <iostream>
+#include <thread>
+
+class ScopedRedirect {
+   public:
+	ScopedRedirect(std::ostream &inOriginal, std::ostream &inRedirect)
+		: mOriginal(inOriginal), mOldBuffer(inOriginal.rdbuf(inRedirect.rdbuf())) {
+	}
+
+	~ScopedRedirect() {
+		mOriginal.rdbuf(mOldBuffer);
+	}
+
+   private:
+	ScopedRedirect(const ScopedRedirect &);
+	ScopedRedirect &operator=(const ScopedRedirect &);
+
+	std::ostream &mOriginal;
+	std::streambuf *mOldBuffer;
+};
+
 int main() {
+	std::string temp_output_string;
+	std::stringstream output_stream;
+	std::cout.rdbuf(output_stream.rdbuf());
+
 	sf::RenderWindow window(sf::VideoMode(1024, 768), "NGine - N64 Engine Powered by Libdragon");
 	window.setFramerateLimit(60);
 	if (!ImGui::SFML::Init(window))
@@ -51,9 +77,14 @@ int main() {
 				ImGui::SameLine();
 				ImGui::InputText("##", input, 255);
 				if (ImGui::Button("Create Project", ImVec2(50, 20))) {
+					console.AddLog("Running 'libdragon init' at '%s'...", input);
+					console.AddLog("Check output on the console.");
+
 					char command[500];
 					snprintf(command, 500, "cd %s\nlibdragon init", input);
-					system(command);
+
+					std::thread(system, command).detach();
+
 					new_project_window_open = false;
 				}
 				ImGui::SameLine();
@@ -62,6 +93,11 @@ int main() {
 				}
 				ImGui::End();
 			}
+		}
+
+		// pulling data from output stream if available
+		while (std::getline(output_stream, temp_output_string, '\n')) {
+			console.AddLog("%s", temp_output_string.c_str());
 		}
 
 		console.Draw("Console", window);
